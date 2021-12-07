5be935f8369ae646f643f61cb4b047fa376725fa
Naresh Choudhari <599choudhari@gmail.com>
2021-12-07 13:47:58 +0530

    Contact section updated

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,128 +5,111 @@ import { Link } from "react-router-dom";
 import { FaPhoneAlt } from "react-icons/fa";
 
 export default function Example() {
-    return (
-        <Disclosure as="nav" className="bg-blue-500 md:px-10">
-            {({ open }) => (
-                <>
-                    <div className="bg-blue-500 sm:px-6 lg:px-8">
-                        <div className="relative flex items-center justify-between h-16">
-                            <div>
-                                <img
-                                    className="hidden lg:block h-8 w-auto text-center"
-                                    src="/Images/bank.jpeg"
-                                    alt="Workflow"
-                                />
-                            </div>
-                            <div className="absolute inset-y-0 left-0 flex items-center lg:hidden">
-                                {/* Mobile menu button*/}
-                                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-black hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                                    <span className="sr-only">
-                                        Open main menu
-                                    </span>
-                                    {open ? (
-                                        <XIcon
-                                            className="block h-6 w-6"
-                                            aria-hidden="true"
-                                        />
-                                    ) : (
-                                        <MenuIcon
-                                            className="block h-6 w-6"
-                                            aria-hidden="true"
-                                        />
-                                    )}
-                                </Disclosure.Button>
-                            </div>
+  return (
+    <Disclosure as="nav" className="bg-blue-500 md:px-10">
+      {({ open }) => (
+        <>
+          <div className="bg-blue-500 sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-16">
+              <div>
+                <img
+                  className="hidden lg:block h-8 w-auto text-center"
+                  src="/Images/bank.jpeg"
+                  alt="Workflow"
+                />
+              </div>
+              <div className="absolute inset-y-0 left-0 flex items-center lg:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-black hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
 
-                            {/* Laptop Navbar */}
+              {/* Laptop Navbar */}
 
-                            <div className="flex-1 flex lg:block items-center justify-center px-2">
-                                <div className="flex items-center">
-                                    <img
-                                        className="lg:hidden block px-2 h-8 w-auto text-center"
-                                        src="/Images/bank.jpeg"
-                                        alt="Workflow"
-                                    />
-                                    <Link
-                                        className=" text-black text-xl font-bold font-myFont"
-                                        to="/"
-                                    >
-                                        Manmandir Bank
-                                    </Link>
-                                </div>
-                                </div>
-                                <div className="hidden lg:block sm:ml-6">
-                                    <div className="flex mx-4 space-x-4">
-                                        <ul className="flex">
-                                            <li className="mx-3 my_li">
-                                                <Link to="/about">
-                                                    About Us
-                                                </Link>
-                                            </li>
-                                            <li className="mx-3 my_li">
-                                                <Link to="/service">
-                                                    Service
-                                                </Link>
-                                            </li>
-                                            <li className="mx-3 my_li">
-                                                <Link to="/plan">Plan</Link>
-                                            </li>
-                                            <li className="mx-3 my_li">
-                                                <Link to="/faq">FAQ</Link>
-                                            </li>
-                                            <li className="mx-3 my_li">
-                                                <Link to="/notice">Notice</Link>
-                                            </li>
-                                            <li className="mx-3 my_li">
-                                                <Link to="/contact">
-                                                    Contact Us
-                                                </Link>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                           
-                            <FaPhoneAlt className="hidden lg:block" />
-                            <div className="hidden lg:block">
-                                <h1 className="my_h1 px-2">
-                                    <a href="tel:+918974562241">8974562241</a>
-                                </h1>
-                            </div>
-                        </div>
-                    </div>
+              <div className="flex-1 flex lg:block items-center justify-center px-2">
+                <div className="flex items-center">
+                  <img
+                    className="lg:hidden block px-2 h-8 w-auto text-center"
+                    src="/Images/bank.jpeg"
+                    alt="Workflow"
+                  />
+                  <Link
+                    className=" text-black text-xl font-bold font-myFont"
+                    to="/"
+                  >
+                    Manmandir Bank
+                  </Link>
+                </div>
+              </div>
+              <div className="hidden lg:block sm:ml-6 my-auto">
+                <div className="flex mx-4 mt-3.5 items-center justify-center text-center">
+                  <ul className="flex">
+                    <li className="mx-3 my_li font-myFont">
+                      <Link to="/about">About Us</Link>
+                    </li>
+                    <li className="mx-3 my_li font-myFont">
+                      <Link to="/service">Service</Link>
+                    </li>
+                    <li className="mx-3 my_li font-myFont">
+                      <Link to="/plan">Plan</Link>
+                    </li>
+                    <li className="mx-3 my_li font-myFont">
+                      <Link to="/faq">FAQ</Link>
+                    </li>
+                    <li className="mx-3 my_li font-myFont">
+                      <Link to="/notice">Notice</Link>
+                    </li>
+                    <li className="mx-3 my_li font-myFont">
+                      <Link to="/contact">Contact Us</Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
 
-                    <Disclosure.Panel className="lg:hidden">
-                        <div className="px-2 pt-2 pb-3 space-y-1 bg-blue-500 text-center">
-                            <Disclosure.Button>
-                                <div className="flex">
-                                    <ul className="flex-col">
-                                        <li className="my_li">
-                                            <Link to="/about">About Us</Link>
-                                        </li>
-                                        <li className="my_li">
-                                            <Link to="/service">Service</Link>
-                                        </li>
-                                        <li className="my_li">
-                                            <Link to="/invest">Invest</Link>
-                                        </li>
-                                        <li className="my_li">
-                                            <Link to="/faq">FAQ</Link>
-                                        </li>
-                                        <li className="mx-3 my_li">
-                                                <Link to="/notice">Notice</Link>
-                                            </li>
-                                        <li className="my_li">
-                                            <Link to="/contact">
-                                                Contact Us
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </Disclosure.Button>
-                        </div>
-                    </Disclosure.Panel>
-                </>
-            )}
-        </Disclosure>
-    );
+              <div className="hidden lg:block -mt-1">
+                <a href="tel:+918974562241">
+                  <FaPhoneAlt className="hidden lg:block" />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="lg:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1 bg-blue-500 text-center">
+              <Disclosure.Button>
+                <div className="flex">
+                  <ul className="flex-col">
+                    <li className="my_li font-myFont">
+                      <Link to="/about">About Us</Link>
+                    </li>
+                    <li className="my_li font-myFont">
+                      <Link to="/service">Service</Link>
+                    </li>
+                    <li className="my_li font-myFont">
+                      <Link to="/invest">Invest</Link>
+                    </li>
+                    <li className="my_li font-myFont">
+                      <Link to="/faq">FAQ</Link>
+                    </li>
+                    <li className="mx-3 my_li font-myFont">
+                      <Link to="/notice">Notice</Link>
+                    </li>
+                    <li className="my_li font-myFont">
+                      <Link to="/contact">Contact Us</Link>
+                    </li>
+                  </ul>
+                </div>
+              </Disclosure.Button>
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  );
 }
--- src/Components/Plan/Plan.js
+++ src/Components/Plan/Plan.js
@@ -5,12 +5,12 @@ const Plan = () => {
         <div className="py-10 min-h-screen">
         <h1 className="md:text-6xl text-4xl font-myFont text-center text-blue-400 py-6">Various Deposit Schemes Of The Bank</h1>
         <div className="container mb-16 text-center">
-        <h1 className="text-center text-blue-500 text-4xl py-6 font-myFont">Attractive interest rates on deposits</h1>
+        <h1 className="text-center text-blue-900 text-4xl py-6 font-myFont">Attractive interest rates on deposits</h1>
         <table class="table table-striped table-bordered container mx-auto border-2 rounded-lg border-black">
         
         <thead>
           <tr>
-            <th scope="col">Duration</th>
+            <th scope="col" >Duration</th>
             <th scope="col">Interest Rate</th>
         
           </tr>
@@ -61,7 +61,7 @@ const Plan = () => {
       </div>
       <div className="container w-full  flex md:flex-row flex-col">
       <div className="md:w-1/2 md:mx-4 w-full text-center mb-10">
-      <h1 className="text-3xl text-center text-blue-500 py-10 font-myFont">Manmandir Deposit Scheme <br/> (Term 5 Years)</h1>
+      <h1 className="text-3xl text-center text-blue-900 py-10 font-myFont">Manmandir Deposit Scheme <br/> (Term 5 Years)</h1>
       <table class="table table-striped table-bordered border-2 rounded-lg border-black">
       <thead>
         <tr>
@@ -120,7 +120,7 @@ const Plan = () => {
     </table>
       </div>
       <div className="md:w-1/2 w-full md:mx-4 text-center mb-10">
-      <h1 className="text-3xl text-center text-blue-500 font-myFont py-10">Manmandir Recurring Deposit Scheme <br /><span className="text-2xl">Lakhpati Yojana (Term 5 Years)</span></h1>
+      <h1 className="text-3xl text-center text-blue-900 font-myFont py-10">Manmandir Recurring Deposit Scheme <br /><span className="text-2xl">Lakhpati Yojana (Term 5 Years)</span></h1>
       <table class="table table-striped table-bordered container mx-auto border-2 rounded-lg border-black">
         <thead>
           <tr>
