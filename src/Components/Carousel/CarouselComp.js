bbb1a701b77dce29697abcd095b977e8d7bd4487
official-akshayjadhav <official.akshayjadhav@gmail.com>
2022-01-08 08:30:25 +0530

    made images responsive

--- src/Components/Carousel/CarouselComp.js
+++ src/Components/Carousel/CarouselComp.js
@@ -1,63 +1,56 @@
-import React from 'react'
-import Carousel from 'react-bootstrap/Carousel';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const CarouselComp = () => {
-  return (
-    <div>
-    <Carousel className="carousel">
-  
-  <Carousel.Item className="w-full">
-    <img
-      className="flex justify-center items-center mx-auto my_bg  img-fluid"
-      src="/Images/poster4.jpg"
-      alt="Fourth slide"
-    />
-  </Carousel.Item>
-   <Carousel.Item> 
-    <img
-      className="flex justify-center items-center mx-auto my_bg  img-fluid"
-      src="/Images/poster5.jpg"
-      alt="Fifth slide"
-    />
-
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="flex justify-center items-center mx-auto my_bg img-fluid"
-      src="/Images/poster1.jpg"
-      alt="First slide"
-    />
-    
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="flex justify-center items-center mx-auto my_bg  img-fluid"
-      src="/Images/poster2.jpg"
-      alt="Second slide"
-    />
-
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="flex justify-center items-center mx-auto my_bg  img-fluid"
-      src="/Images/poster3.jpg"
-      alt="Third slide"
-    />
-
-  </Carousel.Item> 
-  <Carousel.Item> 
-    <img
-      className="flex justify-center items-center mx-auto my_bg  img-fluid"
-      src="/Images/poster6.jpg"
-      alt="Sixth slide"
-    />
-
-  </Carousel.Item> 
-</Carousel>
-    </div>
-  )
-}
+    return (
+        <div>
+            <Carousel className="carousel">
+                <Carousel.Item className="w-full">
+                    <img
+                        className="flex justify-center items-center img-fluid"
+                        src="/Images/poster4.jpg"
+                        alt="Fourth slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="flex justify-center items-center img-fluid"
+                        src="/Images/poster5.jpg"
+                        alt="Fifth slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="flex justify-center items-center img-fluid"
+                        src="/Images/poster1.jpg"
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="flex justify-center items-center img-fluid"
+                        src="/Images/poster2.jpg"
+                        alt="Second slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="flex justify-center items-center img-fluid"
+                        src="/Images/poster3.jpg"
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="flex justify-center items-center img-fluid"
+                        src="/Images/poster6.jpg"
+                        alt="Sixth slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
+        </div>
+    );
+};
 
 export default CarouselComp;
